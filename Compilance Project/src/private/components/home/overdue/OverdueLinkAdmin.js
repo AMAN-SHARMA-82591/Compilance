efa865356900b406ab25a88c4306e1912481755e
aman <aman82591@gmail.com>
2023-09-07 20:07:06 +0530

    Removed Section-1 routing from code.

--- Compilance Project/src/private/components/home/overdue/OverdueLinkAdmin.js
+++ Compilance Project/src/private/components/home/overdue/OverdueLinkAdmin.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import LogInAdmin from '../../../Section-2/LogInAdmin'
-import WeekTable from '../../../Section-1/WeekTable'
+import WeekTable from '../../../Common/WeekTable';
+import WeekTable from '../../../Common/WeekTable';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Button from '@mui/material/Button'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import ButtonGroup from '@mui/material/ButtonGroup'
 
 
@@ -11,27 +12,27 @@ import {Link} from 'react-router-dom'
 function overdueLinkAdmin() {
     return (
         <div>
-                <div className="main-login-admin-weektable-section">
-                        <div className="overdue-arrowback-logo">
-                        <Link to="/">
-                            <Button color="primary" variant="outlined">
-                                <ArrowBackIcon fontSize="large" className="arrow-back-logo" />
-                            </Button>
+            <div className="main-login-admin-weektable-section">
+                <div className="overdue-arrowback-logo">
+                    <Link to="/">
+                        <Button color="primary" variant="outlined">
+                            <ArrowBackIcon fontSize="large" className="arrow-back-logo" />
+                        </Button>
 
-                        </Link>
-                           
-                        </div>
-                        <div className="overdue-weektable-section">
-                                 <WeekTable className="overdue-week-table"/>
-                           
-                        </div>
-                        <div className="overdue-login-admin">
-                            <LogInAdmin />
-                        </div>
-                </div>
-                <div className="practice-section">
+                    </Link>
 
                 </div>
+                <div className="overdue-weektable-section">
+                    <WeekTable className="overdue-week-table" />
+
+                </div>
+                <div className="overdue-login-admin">
+                    <LogInAdmin />
+                </div>
+            </div>
+            <div className="practice-section">
+
+            </div>
         </div>
     )
 }
